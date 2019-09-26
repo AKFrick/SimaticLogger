d42951bd8c44b609f7f0996a9ecd711fb1f4b454
Alex <ak.frick@Outlook.com>
2019-09-26 12:30:13 +0500

    divide view to parts

---
+++ SimaticLogger/ViewModel/MainVM.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Prism.Mvvm;
+
+namespace SimaticLogger
+{
+    public class MainVM : BindableBase
+    {
+        public List<Message> Messages { get; set; }
+        public MainVM()
+        {            
+        }       
+    }
+}
