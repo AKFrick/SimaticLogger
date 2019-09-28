10b2f0ca8334ab7bacc460a5010d540392e9ba8a
Alex <ak.frick@Outlook.com>
2019-09-28 21:43:15 +0500

    some changes

--- SimaticLogger/ModelView/MainVM.cs
+++ SimaticLogger/ModelView/MainVM.cs
@@ -18,13 +18,18 @@ namespace SimaticLogger
                    if (a.NewItems?.Count != 0) Messages.Add(a.NewItems[0] as Message);
                    if (a.OldItems?.Count == 1) Messages.Remove(a.OldItems[0] as Message);
                };
-            InsertNew = new DelegateCommand(() => messageCollector.Collect());
-            DeleteTop = new DelegateCommand(() => Messages.Remove(Messages.First()));
-                                        
+            InsertNew = new DelegateCommand(() =>
+                {
+                    messageCollector.Collect();
+                    BtnContent = "Btn 2";
+                    RaisePropertyChanged(nameof(BtnContent));
+                });
+            DeleteTop = new DelegateCommand(() => Messages.Remove(Messages.First()));                                        
         }
         private MessageCollector messageCollector = new MessageCollector();
         public ObservableCollection<Message> Messages { get; }
         public DelegateCommand InsertNew { get; }
-        public DelegateCommand DeleteTop { get; }        
+        public DelegateCommand DeleteTop { get; }
+        public string BtnContent { get; set; } = "Btn 1";
     }
 }
